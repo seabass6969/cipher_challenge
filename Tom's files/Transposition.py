6a36aac4d6c8e318a7084f665986045ee2bf5be5
Aquatian42 <tomtomdin@gmail.com>
2024-11-07 21:01:08 +0000

    Completed transposition.py

--- Tom's files/Transposition.py
+++ Tom's files/Transposition.py
@@ -1,21 +1,25 @@
 inputtext = (open("text.txt", "r"))
 textlist = inputtext.readlines()
-output = ""
 for i in textlist:
     textstr = i 
 
 groupnum = 6
 group = ""
 grouplist = ""
-grouplist2 = ""
+groupstr = ""
+reset = False
+output = ""
 
 for i in textstr:
     group += i
     if len(group) == groupnum:
         for x in group:
             grouplist += x
-        grouplist2 += grouplist[3] + grouplist[0] + grouplist[2] + grouplist[5] + grouplist[1] + grouplist[4]
-        output += grouplist2
-    grouplist, grouplist2 = [], []
-    group = ""
+        groupstr += grouplist[3] + grouplist[0] + grouplist[2] + grouplist[5] + grouplist[1] + grouplist[4]
+        output += groupstr
+        reset = True
+    if reset:    
+        grouplist, groupstr = [], ""
+        group = ""
+        reset = False
 print(output)
