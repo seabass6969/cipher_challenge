b7314006be93970480410238d76fac114b1891b1
Aquatian42 <tomtomdin@gmail.com>
2024-11-07 20:32:25 +0000

    Added folder for my code so I dont break yours

---
+++ Tom's files/Spaces.py
@@ -0,0 +1,22 @@
+inputtext = (open("text.txt", "r"))
+textlist = inputtext.readlines()
+output = 0
+for i in textlist:
+    textstr = i 
+
+### remove blankspace ###
+textstr2 = ""
+for i in textstr:
+    if not i == " ":
+        textstr2 += i
+
+### add spaces ###
+count = 0
+output = ""
+for i in textstr2:
+    count += 1
+    output += i
+    if count % 6 == 0:
+        output += " "
+
+print(output)
